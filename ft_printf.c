0fd314a15b91002fa013cd66f2dbbe4d9dc874a7
ryhara <aaxouei52@gmail.com>
2023-06-06 23:16:31 +0900

    [Add] ft_printf

--- ft_printf.c
+++ ft_printf.c
@@ -12,7 +12,15 @@
 
 #include "ft_printf.h"
 
-int	ft_printf(const char *, ...)
+int	ft_printf(const char *s, ...)
 {
+	va_list	args;
+	size_t	s_len;
 
+	va_start(args, s);
+	if (s == NULL)
+		return (-1);
+	//処理
+	va_end(args);
+	return ((int)s_len);
 }
